c9cd9583436f2e6d11bee308c1fe9ba997d9d3c7
Alvaro Martinez <amartinez@iaa.es>
2021-10-15 10:25:47 +0200

    cleand unnecesary lines

--- astroaling_VVV.py
+++ astroaling_VVV.py
@@ -28,38 +28,16 @@ chip=1
 VVV='/Users/amartinez/Desktop/PhD/HAWK/GNS_2/VVV/Fields/%s/'%(band)
 file ='/Users/amartinez/Desktop/PhD/HAWK/GNS_2/data/GNS/2021/%s/%s/ims/'%(band,field)
 GNS_stars= '/Users/amartinez/Desktop/PhD/HAWK/GNS_2/data/GNS/2021/%s/%s/data/'%(band,field)
-scripts='/Users/amartinez/Desktop/PhD/HAWK/The_Brick/photometry/scripts/'
 pruebas='/Users/amartinez/Desktop/PhD/HAWK/GNS_2/pruebas/'
-stream = open(scripts+'polywarp.py')
-read_file = stream.read()
-exec(read_file)
 
 
-# In[3]:
 
-
-VVV_all=fits.open(VVV+'Field%s.fits'%(field))[0].data
-l_exp=fits.open(file+'lnx_jitter_%s.fits'%(chip))[0].data
-x,y=VVV_all.shape[0],VVV_all.shape[1]
-VVV_c1=VVV_all[0:round(x/2)-1,0:round(y/2)-1]
-
-
-# In[4]:
-
-
-# p, (pos_img, pos_img_rot) = aa.find_transform(l_exp,VVV_c1,max_control_points=200
-#                                              )
-# print("\nTranslation: (x, y) = ({:.2f}, {:.2f})".format(*p.translation))
-# print("Rotation: %.3f degrees"%(p.rotation * 180.0 / np.pi))
-# print("Rotation: {:.3f} degrees".format(p.rotation * 180.0 / np.pi))
-# print("\nScale factor: {:.4f}".format(p.scale))
-            
 
 
 # In[5]
 dic_gns={}
 dic_f={}
-for i in range(1,3):#loads GNS stars in two separetes list: coordinates and fluxes
+for i in range(3,4):#loads GNS stars in two separetes list: coordinates and fluxes
     dic_gns['gns_c%s'%(i)]=np.loadtxt(GNS_stars+'stars_%s.txt'%(i),usecols=(0,1))
     dic_f['f_c%s'%(i)]=np.loadtxt(GNS_stars+'stars_%s.txt'%(i),usecols=(2))
 # gns_s=np.loadtxt(GNS_stars+'stars_%s.txt'%(chip),usecols=(0,1))
@@ -82,16 +60,11 @@ dic_vvv['vvv_c4']=vvv_s[chip4]*scale
 
 
 
-#%%
-# m,(_,_)= aa.find_transform(gns_s2,vvv_c2,max_control_points=200)
-# print('For chip%s'%(2)+'\n'+"Translation: (x, y) = (%.2f, %.2f)"%(m.translation[0],m.translation[1]))
-# print("Rotation: %.3f degrees"%(m.rotation * 180.0 / np.pi))
-# print("Rotation: %.3f degrees"%(m.rotation * 180.0 / np.pi))
-# print("\nScale factor: %.4f"%(m.scale))
+
 
 # In[7]:
 
-for i in range(1,3):
+for i in range(3,4):
     check_x,check_y=2,2
     while abs(check_x) >1 or abs(check_y)>1  :# only whe tranformation gets better than 1 chip desplacement the coordinates are stored
         m,(_,_)= aa.find_transform(dic_gns['gns_c%s'%(i)],dic_vvv['vvv_c%s'%(i)],max_control_points=450)
