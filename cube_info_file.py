dd6b7d261208a1301977fed0432931f7267763e4
Alvaro iMac <amartinez@iaa.es>
2022-07-06 13:46:08 +0200

    modified the cube_info file with only the cubes with bad frames whitin

--- cube_info_file.py
+++ cube_info_file.py
@@ -25,13 +25,12 @@ common_path = '/Users/alvaromartinez/Desktop/PhD/HAWK/GNS_2/data/GNS/2021/' + ba
 pruebas = '/Users/alvaromartinez/Desktop/PhD/HAWK/GNS_2/pruebas/'
 cubes = '/Users/alvaromartinez/Desktop/PhD/HAWK/GNS_2/data/GNS/2021/' + band + '/' + field +'/cubes/'
 
-with open(common_path + 'cube_info.txt','w') as f:
+with open(common_path + 'cube_info_'+ field +'.txt','w') as f:
     f.write('')
 # Select the cube with bad frames and put them in the 'cube' array
 # Number the bad frames with cero in slices. 
 
-with open(common_path + 'bad_cubes_%s.txt'%(field),'w') as f_bad:
-    f_bad.write('')
+
 
 cube = [11,24,31,34,36,41,45,58]    
 normal =[1,2,3,4,5,6,7,8]
@@ -41,23 +40,14 @@ slices=[[0,2,3,4,5,6,7,8],[0,2,3,4,5,6,7,8],[0,0,0,0,0,6,7,8],[0,0,0,4,5,6,7,8],
 print(len(cube),len(slices))
 if len(cube) != len(slices):
     sys.exit('Check the slices!')
-for i in range(1,71):
-    if i in cube:
-        print(i)
-        with open(common_path + 'cube_info.txt','a') as f:
-            sl = str(slices[cube.index(i)]).replace('[', '').replace(']', '')
-            # f.write('%s \n'%(slices[cube.index(i)]))  
-            f.write('%s \n'%(sl))  
+for i in range(len(cube)):
+    print(i)
+    with open(common_path + 'cube_info_'+ field +'.txt','a') as f:
+        sl = str(slices[cube.index(i)]).replace('[', '').replace(']', '')
+        # f.write('%s \n'%(slices[cube.index(i)]))  
+        f.write('%s, %s \n'%(str(i), sl ))  
         
-        with open(common_path + 'bad_cubes_%s.txt'%(field),'a') as f_bad:
-            f_bad.write(str(i) + '\n')
-            
-    else:
-        with open(common_path + 'cube_info.txt','a') as f:
-            nor =str(normal).replace('[', '').replace(']', '')
-            f.write(str(nor) + '\n') 
-
-
+       
 
 
 
